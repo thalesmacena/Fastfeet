f534555a9636e6aadde7199744b5289386322356
Thales Macena <thalesmacena@gmail.com>
2020-11-05 19:06:14 -0300

    Adicionando Models

--- back-end/src/database/index.js
+++ back-end/src/database/index.js
@@ -1,6 +1,8 @@
 import Sequelize from 'sequelize';
+import User from '../app/models/User';
 import databaseConfig from '../config/database';
 
+const models = [User];
 class Database {
   constructor() {
     this.init();
@@ -8,6 +10,8 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
+
+    models.map((model) => model.init(this.connection));
   }
 }
 

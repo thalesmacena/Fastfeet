8a6b33c346235294aaa24ee3b2a3b2852dde865a
Thales Macena <thalesmacena@gmail.com>
2020-11-05 19:07:43 -0300

    Configurando rotas básicas da aplicação

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/');
+routes.get('/users', UserController.index);
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;

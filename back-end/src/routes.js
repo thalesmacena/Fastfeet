9349d8147218f9e29565328eadeefee7e9f366b2
Thales Macena <thalesmacena@gmail.com>
2020-11-09 17:21:34 -0300

    Criação das rotas de cadastrar e atualizar recipients

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -9,9 +9,11 @@ const routes = new Router();
 routes.get('/users', UserController.index);
 routes.get('/recipients', RecipientsController.index);
 
-routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+routes.post('/users', UserController.store);
+routes.post('/recipients', RecipientsController.store);
+routes.put('/recipients/:id', RecipientsController.update);
 
 export default routes;

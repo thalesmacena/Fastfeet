3579ee24f1f4245b48b5e19152524d56a0d0c8dd
Thales Macena <thalesmacena@gmail.com>
2020-11-19 03:08:48 -0300

    add File and Deliveryman models

--- back-end/src/database/index.js
+++ back-end/src/database/index.js
@@ -1,9 +1,11 @@
 import Sequelize from 'sequelize';
+import Deliveryman from '../app/models/Deliveryman';
+import File from '../app/models/File';
 import Recipients from '../app/models/Recipients';
 import User from '../app/models/User';
 import databaseConfig from '../config/database';
 
-const models = [User, Recipients];
+const models = [User, Recipients, File, Deliveryman];
 class Database {
   constructor() {
     this.init();
@@ -12,7 +14,11 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map((model) => model.init(this.connection));
+    models
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models)
+      );
   }
 }
 

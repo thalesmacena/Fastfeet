dcbbac3bc24dde35db89755e686f9a1aec2b3059
Thales Macena <thalesmacena@gmail.com>
2020-11-19 03:12:03 -0300

    add multer, deliveryman and file controllers and routes

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,19 +1,36 @@
 import { Router } from 'express';
+import multer from 'multer';
+import DeliverymanController from './app/controllers/DeliverymanController';
+import FileController from './app/controllers/FileController';
 import RecipientsController from './app/controllers/RecipientsController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
+const upload = multer(multerConfig);
 const routes = new Router();
 
+// No auth routes
 routes.get('/users', UserController.index);
+
 routes.get('/recipients', RecipientsController.index);
 
 routes.post('/sessions', SessionController.store);
 
+// Auth routes
 routes.use(authMiddleware);
+
 routes.post('/users', UserController.store);
+
 routes.post('/recipients', RecipientsController.store);
 routes.put('/recipients/:id', RecipientsController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/deliverymans', DeliverymanController.index);
+routes.post('/deliverymans', DeliverymanController.store);
+routes.put('/deliverymans/:id', DeliverymanController.update);
+routes.delete('/deliverymans/:id', DeliverymanController.delete);
 
 export default routes;

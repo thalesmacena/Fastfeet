d12cf3dbaa2113cdc76dc233cdeeda5038aff177
Thales Macena <thalesmacena@gmail.com>
2020-11-19 21:29:08 -0300

    add Delivery model

--- back-end/src/database/index.js
+++ back-end/src/database/index.js
@@ -1,11 +1,12 @@
 import Sequelize from 'sequelize';
+import Delivery from '../app/models/Delivery';
 import Deliveryman from '../app/models/Deliveryman';
 import File from '../app/models/File';
 import Recipients from '../app/models/Recipients';
 import User from '../app/models/User';
 import databaseConfig from '../config/database';
 
-const models = [User, Recipients, File, Deliveryman];
+const models = [User, Recipients, File, Deliveryman, Delivery];
 class Database {
   constructor() {
     this.init();

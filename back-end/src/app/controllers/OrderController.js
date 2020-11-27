cb45e69b5f3cdaaffb6e73be5c726db570a1230b
Thales Macena <thalesmacena@gmail.com>
2020-11-27 15:02:08 -0300

    create and update Order process

---
+++ back-end/src/app/controllers/OrderController.js
@@ -0,0 +1,51 @@
+import Delivery from '../models/Delivery';
+import Recipient from '../models/Recipient';
+
+class OrderController {
+  async index(req, res) {
+    const { page = 1, limit = 5 } = req.query;
+    const { deliverymanId } = req.params;
+
+    if (!deliverymanId) {
+      return res.status(400).json({ error: 'Deliveryman does not Exist' });
+    }
+
+    const deliverys = await Delivery.findAll({
+      where: {
+        deliveryman_id: deliverymanId,
+        canceled_at: null,
+        end_date: null,
+      },
+      attributes: [
+        'id',
+        'product',
+        'canceled_at',
+        'cancelable',
+        'start_date',
+        'end_date',
+      ],
+      limit,
+      offset: (page - 1) * 5,
+      include: [
+        {
+          model: Recipient,
+          as: 'recipient',
+          attributes: [
+            'id',
+            'name',
+            'rua',
+            'numero',
+            'complemento',
+            'estado',
+            'cidade',
+            'cep',
+          ],
+        },
+      ],
+    });
+
+    return res.json(deliverys);
+  }
+}
+
+export default new OrderController();
---
+++ back-end/src/app/controllers/OrderStatusController.js
@@ -0,0 +1,143 @@
+import { endOfDay, getHours, isBefore, parseISO, startOfDay } from 'date-fns';
+import { Op } from 'sequelize';
+import * as Yup from 'yup';
+import Delivery from '../models/Delivery';
+import Deliveryman from '../models/Deliveryman';
+import File from '../models/File';
+import Recipient from '../models/Recipient';
+
+class OrderStatusController {
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      start_date: Yup.date(),
+      end_date: Yup.date().when('start_date', (start_date, field) =>
+        start_date ? field : field.required()
+      ),
+      signature_id: Yup.number().when('end_date', (end_date, field) =>
+        end_date ? field.required() : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    // Check if Deliveryman exist
+
+    const deliveryman = await Deliveryman.findOne({
+      where: { user_id: req.userId },
+    });
+
+    if (!deliveryman) {
+      return res.status(400).json({ error: 'Deliveryman not found' });
+    }
+
+    // Check if Delivery Exists
+    const { deliveryId } = req.params;
+
+    const delivery = await Delivery.findByPk(deliveryId);
+
+    if (!delivery) {
+      return res.status(400).json({ error: 'Delivery not found' });
+    }
+
+    // Check if Delivery belongsTo Deliveryman
+    if (delivery.deliveryman_id !== deliveryman.id) {
+      return res
+        .status(401)
+        .json({ error: 'Delivery does not belong to Deliveryman' });
+    }
+
+    // Check if Delivery is closed
+    if (delivery.end_date || delivery.canceled_at) {
+      return res.status(400).json({ error: 'Delivery is closed' });
+    }
+
+    // Check start_date
+    if (req.body.start_date) {
+      const date = parseISO(req.body.start_date);
+      const hour = getHours(date);
+
+      // Check if start_date beetween 08:00 e 18:00h
+      if (hour <= 8 || hour >= 18) {
+        return res.status(400).json({
+          error: 'The withdraw must be made between 8 am and 6 pm',
+        });
+      }
+
+      // Check if the Number of Day Orders
+      const deliveriesAll = await Delivery.findAll({
+        where: {
+          deliveryman_id: deliveryman.id,
+          canceled_at: null,
+          start_date: {
+            [Op.between]: [startOfDay(date), endOfDay(date)],
+          },
+          end_date: null,
+        },
+      });
+
+      if (deliveriesAll.length >= 5) {
+        return res
+          .status(401)
+          .json({ error: 'Deliveryman already has 5 deliveries on this day' });
+      }
+    }
+
+    // check if end_date is after start_date
+    if (req.body.end_date) {
+      const endDate = parseISO(req.body.end_date);
+      const startDate = parseISO(delivery.start_date);
+
+      if (isBefore(startDate, endDate)) {
+        return res
+          .status(400)
+          .json({ error: 'The end_date is before start_date' });
+      }
+    }
+
+    // update delivery
+
+    const updated = await delivery.update(req.body);
+
+    await updated.reload({
+      attributes: ['id', 'product', 'start_date', 'canceled_at', 'end_date'],
+      include: [
+        {
+          model: Deliveryman,
+          as: 'deliveryman',
+          attributes: ['name', 'email'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['name', 'path', 'url'],
+            },
+          ],
+        },
+        {
+          model: Recipient,
+          as: 'recipient',
+          attributes: [
+            'name',
+            'street',
+            'number',
+            'complement',
+            'state',
+            'city',
+            'zip',
+          ],
+        },
+        {
+          model: File,
+          as: 'signature',
+          attributes: ['url', 'name', 'path'],
+        },
+      ],
+    });
+
+    return res.json(updated);
+  }
+}
+
+export default new OrderStatusController();

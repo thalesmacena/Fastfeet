0fa742d107dcb28ceb2f0a41e9ce3a3bacef562c
Thales Macena <thalesmacena@gmail.com>
2021-03-22 08:21:44 -0300

    Add assets and roboto font

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -39,6 +39,8 @@ export default class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
+          <link rel="preconnect" href="https://fonts.gstatic.com">
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
         </Head>
         <body>
           <Main />

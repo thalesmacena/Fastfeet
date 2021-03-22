a29bb41e8f08747e49b227ce1f3683ba00eae8f9
Thales Macena <thalesmacena@gmail.com>
2021-03-22 08:25:22 -0300

    fix on links

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -39,8 +39,11 @@ export default class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
-          <link rel="preconnect" href="https://fonts.gstatic.com">
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />

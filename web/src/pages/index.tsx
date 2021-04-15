69faee732199e45deb3d92176afccdb0482ca78e
Thales Macena <thalesmacena@gmail.com>
2021-04-15 19:00:47 -0300

    config redirect

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,14 @@
+import useUser from '@/lib/useUser';
+
 const Home = () => {
-  return (
-    <h1>Inicio</h1>
-  );
+  useUser({
+    redirectTo: '/dashboard',
+    redirectIfFound: true
+  });
+
+  useUser({ redirectTo: '/signin' });
+
+  return <h1>Inicio</h1>;
 };
 
 export default Home;

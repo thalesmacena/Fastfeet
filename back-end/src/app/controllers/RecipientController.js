6f454e6ee7aa32de9232a57fe4b42681cc8f75f0
Thales Macena <thalesmacena@gmail.com>
2021-04-15 15:34:52 -0300

    Add query params and iLike

--- back-end/src/app/controllers/RecipientController.js
+++ back-end/src/app/controllers/RecipientController.js
@@ -1,11 +1,15 @@
+import { Op } from 'sequelize';
 import * as Yup from 'yup';
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async index(req, res) {
-    const { page = 1 } = req.query;
+    const { page = 1, q = '' } = req.query;
 
     const recipients = await Recipient.findAll({
+      where: {
+        name: { [Op.iLike]: `%${q}%` },
+      },
       limit: 20,
       offset: (page - 1) * 20,
     });

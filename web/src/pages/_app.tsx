af012a0080715858810453cbc46fa12afa24be70
Thales Macena <thalesmacena@gmail.com>
2021-04-15 18:57:16 -0300

    Add Head, ThemeProvider and ToasContainer to app

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ToastContainer } from 'react-toastify';
 import { ThemeProvider } from '../contexts/ThemeContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Fastfeet</title>
+      </Head>
+      <ThemeProvider>
+        <>
+          <Component {...pageProps} />
+          <ToastContainer autoClose={3000} />
+        </>
+      </ThemeProvider>
+    </>
   );
 }
 

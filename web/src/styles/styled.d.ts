8c3867489afd3e5479b6909b09bba2ab85279082
Thales Macena <thalesmacena@gmail.com>
2021-04-16 00:20:22 -0300

    Create dark and light theme

--- web/src/styles/styled.d.ts
+++ web/src/styles/styled.d.ts
@@ -5,7 +5,28 @@ import 'styled-components';
 interface Theme {
   colors: {
     background: string;
+    backgroundDashboard: string;
+
+    primary: string;
+    textInPrimary: string;
+    secondary: string;
+    text: string;
+    textInBackground: string;
+    textInput: string;
+
+    finished: string;
+    finishedBar: string;
+    pending: string;
+    pendingBar: string;
+    withdrawal: string;
+    withdrawalBar: string;
+    error: string;
+    errorBar: string;
   };
+  background: string;
+  modalOverlay: string;
+  boxShadow: string;
+  boxShadowModal: string;
 }
 
 declare module 'styled-components' {
--- web/src/styles/themes.ts
+++ web/src/styles/themes.ts
@@ -1,11 +1,53 @@
-export const darkTheme = {
-  colors: {
-    background: '#191a20'
-  }
-};
-
 export const lightTheme = {
   colors: {
-    background: '#DCDAD7'
-  }
+    background: '#fff',
+    backgroundDashboard: '#F5F5F5',
+    primary: '#7159c1',
+    textInPrimary: '#fff',
+    secondary: '#CCCCCC',
+    text: '#666666',
+    textInBackground: '#444444',
+    textInput: '#999999',
+
+    finished: '#2CA42B',
+    finishedBar: '#DFF0DF',
+    pending: '#C1BC35',
+    pendingBar: '#F0F0DF',
+    withdrawal: '#4D85EE',
+    withdrawalBar: '#BAD2FF',
+    error: '#DE3B3B',
+    errorBar: '#FAB0B0'
+  },
+  background:
+    'linear-gradient(90deg, rgba(113,89,193,1) 0%, rgba(125,64,231,1) 100%);',
+  modalOverlay: 'rgba(74, 74, 74, 0.8)',
+  boxShadow: '0 0 2px 1px rgb(0 0 0 / 33%)',
+  boxShadowModal: '0 0 3.75rem rgba(0, 0, 0, 0.05)'
+};
+
+export const darkTheme = {
+  colors: {
+    background: '#1F1F1F',
+    backgroundDashboard: '#121212',
+    primary: '#7159c1',
+    textInPrimary: '#fff',
+    secondary: '#03DAC6',
+    text: '#CECECE',
+    textInBackground: '#F8F8F8',
+    textInput: '#F4F4F4',
+
+    finished: '#2CA42B',
+    finishedBar: '#DFF0DF',
+    pending: '#C1BC35',
+    pendingBar: '#F0F0DF',
+    withdrawal: '#4D85EE',
+    withdrawalBar: '#BAD2FF',
+    error: '#DE3B3B',
+    errorBar: '#FAB0B0'
+  },
+  background:
+    'linear-gradient(90deg, rgba(18, 18, 18, 1) 0%, rgba(31, 31, 31,1) 100%);',
+  modalOverlay: 'rgba(74, 74, 74, 0.8)',
+  boxShadow: '0 0 2px 1px rgb(0 0 0 / 33%)',
+  boxShadowModal: '0 0 3.75rem rgba(0, 0, 0, 0.05)'
 };

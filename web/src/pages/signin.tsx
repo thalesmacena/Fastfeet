0894943554e43217c3c88f6ecd27dc67c3e23673
Thales Macena <thalesmacena@gmail.com>
2021-04-16 21:12:49 -0300

    add redirect if found

--- web/src/pages/signin.tsx
+++ web/src/pages/signin.tsx
@@ -1,4 +1,11 @@
+import useUser from '@/lib/useUser';
+
 const SignIn = () => {
+  useUser({
+    redirectTo: '/dashboard/deliveries',
+    redirectIfFound: true
+  });
+
   return <h1>Login</h1>;
 };
 

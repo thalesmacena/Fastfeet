9f4421da9c9e579905a212b686b316003d6ed3ea
Thales Macena <thalesmacena@gmail.com>
2021-04-16 21:12:30 -0300

    add cursor pointer

--- web/src/styles/components/ThemeSwitch.tsx
+++ web/src/styles/components/ThemeSwitch.tsx
@@ -2,11 +2,11 @@ import { MdBrightnessHigh, MdBrightnessLow } from 'react-icons/md';
 import styled from 'styled-components';
 
 export const LightSwitch = styled(MdBrightnessHigh)`
-  cursor: 'pointer';
+  cursor: pointer;
   color: ${({ theme }) => theme.colors.switch};
 `;
 
 export const DarkSwitch = styled(MdBrightnessLow)`
-  cursor: 'pointer';
+  cursor: pointer;
   color: ${({ theme }) => theme.colors.switch};
 `;

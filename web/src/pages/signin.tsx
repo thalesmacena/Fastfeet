c298b20d1969a680a9f5bb482539943c9172abb4
Thales Macena <thalesmacena@gmail.com>
2021-04-17 00:55:05 -0300

    Create Login Page

--- web/src/pages/signin.tsx
+++ web/src/pages/signin.tsx
@@ -1,12 +1,132 @@
+import Input from '@/components/InputText';
+import { LoadingSpin } from '@/components/LoadingSpin';
+import { ThemeContext } from '@/contexts/ThemeContext';
 import useUser from '@/lib/useUser';
+import { api, loginApi } from '@/services/api';
+import { Container, Modal } from '@/styles/pages/SignIn';
+import { Form } from '@unform/web';
+import { useRouter } from 'next/dist/client/router';
+import { useContext, useRef, useState } from 'react';
+import { toast } from 'react-toastify';
+import * as Yup from 'yup';
+
+interface LoginData {
+  email: string;
+  password: string;
+}
 
 const SignIn = () => {
+  const { theme } = useContext(ThemeContext);
+  const formRef = useRef(null);
+  const [isInvalidLogin, setIsInvalidLogin] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const Router = useRouter();
+
   useUser({
     redirectTo: '/dashboard/deliveries',
     redirectIfFound: true
   });
 
-  return <h1>Login</h1>;
+  const handleSubmit = async (data: LoginData) => {
+    setIsLoading(true);
+
+    try {
+      formRef.current.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email('Insira um email válido')
+          .required('O email é obrigatório'),
+        password: Yup.string()
+          .min(6, 'A senha precisa ter no minimo 6 caracteres')
+          .required('A senha é obrigatória')
+      });
+
+      await schema.validate(data, {
+        abortEarly: false
+      });
+
+      const response = await loginApi.post('login', {
+        email: data.email,
+        password: data.password
+      });
+
+      if (!response.data.admin) {
+        await loginApi.get('logout');
+        throw new Error('admin');
+      }
+
+      api.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+
+      Router.push('dashboard/deliveries');
+    } catch (err) {
+      setIsLoading(false);
+
+      if (err instanceof Yup.ValidationError) {
+        const validationErrors = {};
+
+        err.inner.forEach((error) => {
+          validationErrors[error.path] = error.message;
+        });
+        formRef.current.setErrors(validationErrors);
+      } else {
+        setIsInvalidLogin(true);
+        if (err.message === 'admin') {
+          toast.error(
+            'É Preciso ser um provedor de serviços para acessar essa aplicação, baixe a versão mobile'
+          );
+        } else {
+          toast.error('Login inválido, verifique seus dados');
+        }
+
+        setTimeout(() => {
+          setIsInvalidLogin(false);
+        }, 2000);
+      }
+    }
+  };
+
+  return (
+    <Container>
+      <Modal>
+        <img
+          src={`/${theme === 'light' ? 'logo-light.svg' : 'logo-dark.svg'}`}
+          alt="Fastfeet"
+        />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <div>
+            <strong>SEU E-MAIL</strong>
+            <Input name="email" type="email" placeholder="exemplo@email.com" />
+          </div>
+
+          <div>
+            <strong>SUA SENHA</strong>
+            <Input
+              name="password"
+              type="password"
+              placeholder="*************"
+            />
+          </div>
+
+          {isInvalidLogin ? (
+            <button disabled type="submit">
+              Login Inválido
+            </button>
+          ) : (
+            <button type="submit">
+              {isLoading ? (
+                <LoadingSpin size={16} color="#fff" spinWidth={5} />
+              ) : (
+                'Entrar no sistema'
+              )}
+            </button>
+          )}
+        </Form>
+      </Modal>
+    </Container>
+  );
 };
 
 export default SignIn;
---
+++ web/src/styles/pages/SignIn.tsx
@@ -0,0 +1,88 @@
+import { darken, lighten } from 'polished';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100%;
+  background: ${({ theme }) => theme.background};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Modal = styled.div`
+  background: ${({ theme }) => theme.colors.background};
+  width: 100%;
+  max-width: 360px;
+  padding: 60px 30px;
+  text-align: center;
+
+  img {
+    width: 240px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 1.875rem;
+
+    div {
+      display: flex;
+      flex-direction: column;
+      margin: 0.625rem 0 0.875rem 0;
+      text-align: left;
+
+      strong {
+        font: 700 0.875rem 'Inter', sans-serif;
+        color: ${({ theme }) => theme.colors.textInBackground};
+        margin: 0 0 0.875rem;
+      }
+
+      input {
+        background: ${({ theme }) => theme.colors.background};
+        border: 0;
+        border-radius: 4px;
+        height: 2.75rem;
+        padding: 0 0.9rem;
+        color: ${({ theme }) => theme.colors.textInBackground};
+        border: 2px solid ${({ theme }) => theme.colors.divider};
+        font: 400 1rem 'Inter', sans-serif;
+
+        &:focus {
+          border-color: ${({ theme }) => lighten(0.03, theme.colors.primary)};
+        }
+
+        &::placeholder {
+          color: ${({ theme }) => theme.colors.textInput};
+        }
+      }
+
+      span {
+        color: ${({ theme }) => theme.colors.error};
+        align-self: flex-start;
+        margin: 0 0 10px;
+        font-weight: bold;
+      }
+    }
+
+    button {
+      margin: 5px 0 0;
+      height: 45px;
+      background: ${({ theme }) => theme.colors.primary};
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.textInPrimary};
+      border: 0;
+      border-radius: 4px;
+      font-size: 1rem;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${({ theme }) => darken(0.03, theme.colors.primary)};
+      }
+
+      &:disabled {
+        background: ${({ theme }) => theme.colors.error};
+        cursor: auto;
+      }
+    }
+  }
+`;
